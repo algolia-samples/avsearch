2bce355d14624865535c82a7e1d1bc08d8b47680
Chuck Meyer <chuck.meyer@algolia.com>
2022-11-16 21:32:03 -0500

    feat: massive frontend refactor to include query suggestions

--- examples/avsearch/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import '@algolia/autocomplete-theme-classic';
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
